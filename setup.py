cd7b97d6cd2eed34a2b7c45d5532581913e997cd
Yaroslav Polyakov <yaroslaff@gmail.com>
2023-02-12 18:54:38 +0700

    version number in help

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 from setuptools import setup
 import os
 import sys
+from importlib.machinery import SourceFileLoader
+
+
+def get_version(path):
+    foo = SourceFileLoader(os.path.basename(path),"ncdu-compare").load_module()
+    return foo.version
+
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+version = get_version('./ncdu_compare')
 
 setup(name='ncdu-compare',
-      version='0.0.3',
+      version=version,
       description='ncdu-compare',
       url='https://github.com/yaroslaff/ncdu-compare',
       author='Yaroslav Polyakov',
